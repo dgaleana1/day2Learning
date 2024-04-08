3f189630d294bf38fd88405f0c2a16f531746307
Galeana <dgaleana1@cps.k12.il.us>
2024-04-08 13:29:23 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# #order of execution
+# # in python
+
+# print("hello world")
+# # strings are surrounded by quotes '' or ""
+# # be consistent with the quotes you use
+# print("order of execution")
+# print("in python")
+# print("*"*20)
+
+# # variables are used to store data
+# # variables are created when you assign a value to it
+# # variables are case sensitive
+
+# price = 10
+# # interger variable
+# name = "John" 
+# # string variable
+# rating = 4.9
+# # float variable
+# is_published = True 
+# # boolean variable
+# # all variables start with lowercase letter or underscore
+# # use camel case if you want to start with a capital letter
+# # on the next word
+# playerBulls = "micheal jordan"
+
+# # Concatentation to join variables in a sentence
+# print(name + "is a basketball player")
+# # the plus sign is used to concatenate strings
+# print(name + "has a rating of" + str(rating))
+# print("the price of the book is" + str(price))
+# # use the str() function to convert a number to a string
+
+# # getting input from the user
+# name = input("what is your name?")
+# age = input("what is your age?")
+# print("hello" + name + "you are" + age + "years old")
+
+# ask two questions from the user
+# person's name and favorite color
+# then print a message like "moses likes blue"
+name = input("what is your name?")
+favorite_color = input("what is your favorite color?")
+print(name + "likes" + favorite_color)
