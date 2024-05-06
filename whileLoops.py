421e6a7508457db2d47fc54ec63517a5f61febbc
Galeana <dgaleana1@cps.k12.il.us>
2024-05-06 12:12:21 -0500

    while loops

---
+++ whileLoops.py
@@ -0,0 +1,29 @@
+# nums = [1, 2, 3, 4, 5]
+
+# for num in nums:
+#     for letter in "abc":
+#         print(num, letter)
+
+# # ranges
+# for i in range(1, 421):
+#     print(i)
+# # first number is inclusive, second number is exclusive
+
+# while loops
+x = 0
+
+# while True:
+#     if x == 5:
+#         break
+#     print(x)
+#     x += 1
+#     # this is an incremmentor (x += 1)
+
+# while True:
+#     print("Enter your name:")
+#     name = input()
+#     if name == 'Nai':
+#         print("Your name is " + name)
+#         break
+#     else:
+#         print("Thank you!")
